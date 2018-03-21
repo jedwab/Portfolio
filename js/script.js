a4506ce03409a1287e87d7938bdeeeaa766b3d4a
Justyna Bobrowska <justynabobrowska88@gmail.com>
2018-03-21 22:47:12 +0100

    add styles to technology section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ $('.js-tilt').tilt({
 
 
 });
+
+$(document).ready(function(){       
+    //sticky navbar
+    var scroll_start = 0;  
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
+     if (startchange.length){
+    $(document).scroll(function() { 
+       scroll_start = $(this).scrollTop();
+       if(scroll_start > offset.top) {
+           $(".about").css('background-color', '#30c7ec').css('transition', 'all 3s');
+           $("span").css('color', "white");
+        } else {
+           $('.about').css('background-color', '#fff').css('color','black');
+           $("span").css('color', '#9A19B3');
+        }
+    });
+     }
+ });
+ window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('i', {duration: 1000}, 200);
