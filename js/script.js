65d8f0ba27a23568f6b04ff524365fbf674d03e6
Justyna Bobrowska <justynabobrowska88@gmail.com>
2018-03-24 18:54:26 +0100

    delete unnecessary files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $('.js-tilt').tilt({
 
     glare: true,
-    maxGlare: .5,
+    maxGlare: 40,
 
 
 });
@@ -16,7 +16,7 @@ $(document).ready(function(){
        scroll_start = $(this).scrollTop();
        if(scroll_start > offset.top) {
            $(".about").css('background-color', '#30c7ec').css('transition', 'all 3s');
-           $("span").css('color', "white");
+           $("span").css('color', "white").css('font-weight','700');
         } else {
            $('.about').css('background-color', '#fff').css('color','black');
            $("span").css('color', '#9A19B3');
@@ -27,3 +27,5 @@ $(document).ready(function(){
  window.sr = ScrollReveal({ reset: true });
 
 sr.reveal('.technology', {duration: 1000}, 200);
+
+
