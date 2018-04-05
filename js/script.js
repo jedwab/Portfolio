e7ca1d8a4b241f44817e66ef94c6fcccbbbdfea5
Justyna Bobrowska <justynabobrowska88@gmail.com>
2018-04-05 14:28:55 +0200

    beautify scss

--- js/script.js
+++ js/script.js
@@ -3,55 +3,55 @@ $('.js-tilt').tilt({
     maxGlare: .5,
 });
 
-$(document).ready(function(){       
+$(document).ready(function () {
 
-    var scroll_start = 0;  
+    var scroll_start = 0;
     var startchange = $('#startchange');
     var offset = startchange.offset();
-     if (startchange.length){
-    $(document).scroll(function() { 
-       scroll_start = $(this).scrollTop();
-       if(scroll_start > offset.top) {
-           $(".about").css('background-color', '#30c7ec').css('transition', 'all 3s');
-           $("span").css('color', "white").css('font-weight', '700').css('transition', 'all 1s');
-        } else {
-           $('.about').css('background-color', '#fff').css('color','black').css('transition', 'all 3s');
-           $("span").css('color', '#9A19B3').css('transition', 'all 1s');
-        }
-    });
-     }
- });
- 
- window.sr = ScrollReveal({ reset: true });
- sr.reveal('.technology', {duration: 1000}, 200);
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $(".about").css('background-color', '#30c7ec').css('transition', 'all 3s');
+                $("span").css('color', "white").css('font-weight', '700').css('transition', 'all 1s');
+            } else {
+                $('.about').css('background-color', '#fff').css('color', 'black').css('transition', 'all 3s');
+                $("span").css('color', '#9A19B3').css('transition', 'all 1s');
+            }
+        });
+    }
+});
 
- $('.collapsed').on('click', function(event) {
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.technology', { duration: 1000 }, 200);
+
+$('.collapsed').on('click', function (event) {
     if (window.matchMedia('(max-width: 576px)').matches) {
         $('.box-1').css('margin-top', '0');
-        $('.box-2').css( 'margin-top', '17px');
+        $('.box-2').css('margin-top', '17px');
         $('.box').css('height', '80px');
         $('.my-photo').css('margin-top', '50px');
     }
 });
 
-$(".o-mnie").click(function() {
+$(".o-mnie").click(function () {
     $('html, body').animate({
         scrollTop: $(".about").offset().top
     }, 1000);
 });
 
-$(".technologia").click(function() {
+$(".technologia").click(function () {
     $('html, body').animate({
         scrollTop: $("#technology").offset().top
     }, 1000);
 });
 
-$(".kontakt").click(function() {
+$(".kontakt").click(function () {
     $('html, body').animate({
         scrollTop: $("#kontakt").offset().top
     }, 1000);
 });
-$(".projekty").click(function() {
+$(".projekty").click(function () {
     $('html, body').animate({
         scrollTop: $("#projekty").offset().top
     }, 1000);
